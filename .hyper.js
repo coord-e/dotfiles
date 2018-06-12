8317144d996bf3515236bb720c4ca402201b38ca
coord.e <me@coord-e.com>
2018-06-13 07:08:07 +0900

    Use patched font to use icons in vim

--- .hyper.js
+++ .hyper.js
@@ -17,7 +17,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"Fira Mono for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Hack Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
