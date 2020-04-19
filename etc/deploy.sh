e1f67396af73ea4cb85c70c4a832d9cfbe85f35a
coord.e <me@coord-e.com>
2020-04-19 12:10:41 +0000

    Fix: Use appropriate shebang

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 find . -name '.*' ! -path . ! -path ./.git ! -path ./.gitignore | xargs -I% ln -frsnv % ~/
 find config/ -maxdepth 1 ! -path config/ | xargs -I% ln -frsnv % ~/.config
